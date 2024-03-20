0fd9205edf91367c922cc6e76ea3d64bb07ef7f8
Joel <jen21092@byui.edu>
2024-03-20 11:36:24 -0600

    setup game start screen

---
+++ src/js/trainer.mjs
@@ -0,0 +1,193 @@
+import { getLocalStorage, setLocalStorage, renderWithTemplate, setClick } from "./utils.mjs";
+
+// Set up new trainers
+export default async function newTrainer(name, pass = "secret") {
+  const p = getLocalStorage(name.toLowerCase());
+  try {
+    if (!p || p.name === "") {
+      setLocalStorage(name, {
+        "name": name,
+        "pass": pass,
+        "wins": 0,
+        "losses": 0,
+        "coins": 0, 
+        "skirmishCards": {}
+      });
+      // Clear data if user has not completed registration 
+      // localStorage.removeItem("trainers");
+      // Take user through registration
+      // newTrainerModal();
+    }
+  } catch (error) {
+    console.error("Trainer already exists: ", error);
+  }
+}
+
+export async function updateSkirmishCards(name, pokeData, poke) {
+  const trainer = getLocalStorage(name);
+
+  if(trainer) {
+    await pokeData.map(card => {
+      const key = Object.keys(card);
+      console.log(key);
+      // console.log(pokeData);
+      
+      if (key[0] in trainer.skirmishCards) {
+        console.log(`${key[0]} already exists, cannot add.`);
+      } else {
+        console.log(`Add ${key[0]}`);
+        trainer.skirmishCards[key] = card;
+      }
+    });
+    setLocalStorage(name, trainer);
+  } else {
+    console.log(`${name} not in localstorage.`);
+  }
+}
+
+// Remove registration modal if user declines registration
+function closeModal() {
+  document.querySelector("#register-name").value = "declined";
+  document.querySelector("#register-email").value = "declined@example.com";
+  document.querySelector("#registered").click();
+}
+
+// Gets user input and saves to localStorage
+function addTrainer() {
+  const userName = document.querySelector("#trainer-name").value;
+  
+  setLocalStorage("trainers", {
+    "name": userName
+  });
+}
+
+// export function verifyRegistration() {
+//   const thanksContainer = document.querySelector("#thanks-container");
+//   thanksContainer.style.display = "flex";
+//   thanksContainer.classList.add("trainer-fade");
+//   thanksContainer.addEventListener("animationend", () => {
+//     thanksContainer.style.display = "none";
+//   });
+// }
+
+// Add event listeners to modal buttons
+function newTrainerModal() {
+  const modalParent = document.querySelector(".divider");
+  const multi = false;
+  
+  renderWithTemplate(renderModal, modalParent, undefined, () => {
+    // Add eventlisteners after modal is created
+    setClick("#added", addTrainer);
+    // setClick("#not-registered", closeModal);
+  });
+}
+
+// Create registration modal
+function renderModal() {
+  return `
+    <section class="trainer-modal">
+      <form class="trainer-form">
+        <fieldset id="trainer-num">
+          <legend>Welcome to PokeSkirmish!</legend>
+          <label class="trainer-label"
+            ><span class="label-span">How many trainers are there? </span
+            ><input type="text" id="trainer-name" name="name" autofocus required
+          /></label>
+        </fieldset>
+
+        <fieldset>
+          <legend>Account Registration</legend>
+          <label class="trainer-label"
+            ><span class="label-span">Name </span
+            ><input type="text" id="trainer-name" name="name" autofocus required
+          /></label>
+        </fieldset>
+
+        <button class="modal-btn" id="not-registered">No Thanks</button>
+        <input type="submit" value="Start Game" class="modal-btn" id="start-game" />
+      </form>
+    </section>
+  `;
+}
+
+// Show a half-transparent DIV to "shadow" the page
+// (the form is not inside, but near it, because it shouldn't be half-transparent)
+// function openNewsletter() {
+//   let nlContainer = document.createElement("div");
+//   nlContainer.id = "newsletter-container";
+
+//   // make the page unscrollable while the modal form is open
+//   document.body.style.overflowY = "hidden";
+
+//   document.body.append(nlContainer);
+//   nlContainer.style.display = "block";
+// }
+
+// function closeNewsletter() {
+//   document.querySelector("#newsletter-container").remove();
+//   document.body.style.overflowY = "";
+// }
+
+// // function showPrompt(callback) {
+// function showPrompt() {
+//   openNewsletter();
+//   let form = document.querySelector(".newsletter-form");
+//   let nlClose = document.querySelector("#newsletter-close");
+//   let nlModal = document.querySelector(".newsletter-modal");
+//   document.body.append(nlModal);
+//   nlModal.style.display = "flex";
+
+//   function complete(value) {
+//     closeNewsletter();
+//     nlModal.style.display = "none";
+//     document.onkeydown = null;
+//     // callback(value);
+//     console.log(value);
+//   }
+
+//   form.onsubmit = function() {
+//     addThanks();
+    
+//     let value = form.email.value;
+
+//     if (value == "") return false; // ignore empty submit
+  
+//     setLocalStorage("newsletter", {
+//       "email": value
+//     });
+//     complete(value);
+//     return false;
+//   };
+
+//   nlClose.addEventListener("click", function() {
+//     complete(null);
+//   });
+
+//   form.elements[1].focus();
+// }
+
+// const nlo = document.querySelector("#newsletter-open")
+
+// nlo.onclick = function() {
+//   nlo.style.display = "none";
+//   showPrompt();
+// };
+
+// function addThanks() {
+//   const mission = document.querySelector(".mission");
+
+//   // Create a new div element
+//   const thanks = document.createElement("section");
+//   thanks.id = "thanks-container";
+
+//   const nlThanks = document.createElement("div");
+//   nlThanks.id = "newsletter-thanks";
+//   nlThanks.textContent = "You're on the list!";
+
+//   thanks.appendChild(nlThanks);
+
+//   // Append the new div to the parent of mission
+//   mission.parentNode.insertBefore(thanks, mission.nextSibling);
+  
+//   thanks.style.display = "block";
+// }
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,5 +1,8 @@
 // getLocalStorage, setLocalStorage, setClick, and getParam, renderWithTemplate, loadTemplate, loadHeaderFooter sourced from WDD330 Team Website project
 
+import { storePokeData } from "./pokebank.mjs";
+import newTrainer, { updateSkirmishCards } from "./trainer.mjs";
+
 // retrieve data from localstorage
 export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
@@ -17,12 +20,129 @@ export function setClick(selector, callback) {
   document.querySelector(selector).addEventListener("click", callback);
 }
 
+export function setClickAll(selector, callback) {
+  const btns = document.querySelectorAll(selector);
+  btns.forEach(btn => {
+    btn.addEventListener("touchend", (event) => {
+      event.preventDefault();
+      const action = btn.getAttribute("data-action");
+      callback(action, event);
+    });
+    btn.addEventListener("click", (event) => {
+      const action = btn.getAttribute("data-action");
+      callback(action, event);
+    });
+  });
+}
+
+export async function addActions(action, event) {
+  const buttonName = event.target.name;
+  const input = document.querySelector(`.trainer-input[name="${buttonName}"]`);
+  // const fieldset = button.closest(".modal-fieldset"); 
+  // const input = fieldset.querySelector(".trainer-input");
+  // const input = document.querySelector(button); 
+  // const inputValue = input.value;
+  let trainer;
+  switch(action) {
+    case "welcomeTrainer":
+      document.querySelector("#trainer-num").style.display = "none";
+      document.querySelector("#trainer1Fieldset").style.display = "block";
+      break;
+    case "addTrainer1":
+      console.log("addTrainer1");
+      // Get name of Trainer1 & save to localStorage
+      newTrainer(input.value);
+      // Get random Pokemon for Trainer1
+      trainer = await storePokeData(10);
+      // console.log(trainer);
+      // Add Pokemon to Trainer1 profile
+      updateSkirmishCards(input.value, trainer);
+      // Hide Trainer1Fieldset
+      document.querySelector("#trainer1Fieldset").style.display = "none";
+      // Show Trainer2Fieldset
+      document.querySelector("#trainer2Fieldset").style.display = "block";
+      break;
+    case "loginTrainer1":
+      console.log("loginTrainer1");
+      document.querySelector("#trainer1Fieldset").style.display = "none";
+      document.querySelector("#login1Fieldset").style.display = "block";
+      break;
+    case "skirmish1":
+      console.log("skirmish1");
+      document.querySelector("#trainer1Fieldset").style.display = "none";
+      document.querySelector("#start-btn").style.display = "block";
+      break;
+    case "addTrainer1Login":
+      console.log("addTrainer1Login");
+      document.querySelector("#login1Fieldset").style.display = "none";
+      document.querySelector("#trainer2Fieldset").style.display = "block";
+      break;
+    case "skirmish1Login":
+      console.log("skirmish1Login");
+      document.querySelector("#login1Fieldset").style.display = "none";
+      document.querySelector("#start-btn").style.display = "block";
+      break;
+    case "loginTrainer2":
+      console.log("loginTrainer2");
+      
+      document.querySelector("#trainer2Fieldset").style.display = "none";
+      document.querySelector("#login2Fieldset").style.display = "block";
+      break;
+    case "skirmish2":
+      console.log("skirmish2");
+      // Get name of Trainer1 & save to localStorage
+      newTrainer(input.value);
+      // Get random Pokemon for Trainer1
+      trainer = await storePokeData(10);
+      // console.log(trainer);
+      // Add Pokemon to Trainer1 profile
+      updateSkirmishCards(input.value, trainer);
+      document.querySelector("#trainer2Fieldset").style.display = "none";
+      document.querySelector("#start-btn").style.display = "block";
+      break;
+    case "skirmish2Login":
+      console.log("skirmish2Login");
+      document.querySelector("#login2Fieldset").style.display = "none";
+      document.querySelector("#start-btn").style.display = "block";
+      break;
+    default:
+      console.error(`Error: bad ${event}`);
+      break;
+  }
+}
+
+export function loadAnimations() {
+  setTimeout(() => {
+    throwBall();
+  }, 3000);
+  
+  setTimeout(() => {
+    document.querySelector("form").style.display = "block";
+  }, 3200);
+}
+
+// Example function to apply riseOffscreen animation
+function throwBall() {
+  document.querySelectorAll(".bg").forEach(element => {
+    // Remove previous animation styles if necessary
+    element.style.animation = "throwBall .4s forwards";
+    element.addEventListener("animationend", (e) => {
+      if (e.animationName === "throwBall") {
+        element.style.display = "none";
+      }
+    });
+  });
+
+  const form = document.querySelector("form");
+  // form.style.display = "block";
+  form.style.animation = "popForm .4s forwards";
+}
+
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   return urlParams.get(param);
 }
-
 
 export async function renderWithTemplate(
   templateFn,
